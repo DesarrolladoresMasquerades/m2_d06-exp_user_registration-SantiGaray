23a611b92d579ef8ed1d580dd94980a49cefc2b7
Santiago <santiagofgaray@gmail.com>
2022-02-14 17:15:55 +0100

    update class

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,6 +3,7 @@ const User = require('../models/User.model');
 const router = express.Router();
 const saltRounds = 5;
 const bcrypt = require('bcrypt');
+const res = require('express/lib/response');
 
 router.route('/signup')
 .get( (req, res) => {
@@ -19,10 +20,12 @@ router.route('/signup')
 	}
 	User.findOne({username})
 	.then((user) =>{
-		if(user && user.username) {res.render("signup", {errorMessage: "User already taken!"}) 
+		if(user && user.username) {
+			res.render("signup", {errorMessage: "User already taken!"});
+		throw new Error("Validation Error 1") // BEST WAY!!!!
 		console.log("Exist: ",user) }
 		
-		else{
+		//else{
 		const salt = bcrypt.genSaltSync(saltRounds) // we bring bcrypt and we are telling it to create randomly the hashedPwd
 		const hashedPwd = bcrypt.hashSync(password, salt)
 
@@ -30,18 +33,55 @@ router.route('/signup')
 		.then((newUser) => {
 			console.log("New User: ", newUser)
 			res.redirect("/")
-		})}
+		})//}
 	
 
 	});
 });
 
-router.get('/login', (req, res, next) => {
-	res.render('login');
-});
+router
+  .route("/login")
+  .get((req, res) => {
+    res.render("login");
+  })
+  .post((req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if (!username || !password) {
+      res.render("login", { errorMessage: "All filds are required" });
+      throw new Error("Validation error");
+    }
+
+    User.findOne({ username })
+      .then((user) => {
+        if (!user) {
+          res.render("login", { errorMessage: "Incorrect credentials!" });
+          throw new Error("Validation error");
+        }
+
+        const isPwCorrect = bcrypt.compareSync(password, user.password);
+        if (isPwCorrect) {
+          req.session.currentUserId = user._id
+          res.redirect("/auth/profile");
+          //res.render("login", { errorMessage: "You are correctly logged in!" })
+        } else {
+          res.render("login", { errorMessage: "Incorrect credentials!" });
+        }
+      })
+      .catch((error) => console.log(error));
+  });
 
 router.get('/profile', (req, res) => {
-	res.render('profile');
+  const id = req.session.currentUserId;
+  User.findById(id)
+  .then((user) => res.render("profile", user))
+  .catch(err=>console.log(err));
 });
+
+router.get("/logout", (req, res) => {
+  req.session.destroy((err)=>res.redirect("/")
+  )
+})
 
 module.exports = router;
