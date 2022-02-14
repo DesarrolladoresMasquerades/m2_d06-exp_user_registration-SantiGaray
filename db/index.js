f996d814d401e61162121c524416e4a06ab5077f
Santiago <santiagofgaray@gmail.com>
2022-02-14 14:53:32 +0100

    update class

--- db/index.js
+++ db/index.js
@@ -1,14 +1,10 @@
 const mongoose = require("mongoose");
+require("dotenv").config()
 
-const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/express-basic-auth";
+const MONGO_URI = process.env.MONGODB_URI || `mongodb+srv://${process.env.MG_USERNAME}:${process.env.MG_PWD}@cluster0.6fts6.mongodb.net/express-basic-auth`;
 
 mongoose
-  .connect(MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true
-  })
+  .connect(MONGO_URI)
   .then((x) => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
   })
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,11 +1,39 @@
 const express = require('express');
 const User = require('../models/User.model');
 const router = express.Router();
-const saltRound = 10;
+const saltRounds = 5;
 const bcrypt = require('bcrypt');
 
-router.get('/signup', (req, res, next) => {
+router.route('/signup')
+.get( (req, res) => {
 	res.render('signup');
+})
+.post((req, res) => {;
+	const username = req.body.username;
+	const password = req.body.password;
+	
+	// Check the form is NOT empty
+	if(!username || !password) { 
+		res.render("signup", {errorMessage: "All fields are required"})
+		// return // this if is a killswitch, it tells me that the body will execute if this if is passed. by body means what is next.(User.find, User.create)
+	}
+	User.findOne({username})
+	.then((user) =>{
+		if(user && user.username) {res.render("signup", {errorMessage: "User already taken!"}) 
+		console.log("Exist: ",user) }
+		
+		else{
+		const salt = bcrypt.genSaltSync(saltRounds) // we bring bcrypt and we are telling it to create randomly the hashedPwd
+		const hashedPwd = bcrypt.hashSync(password, salt)
+
+		User.create({ username, password: hashedPwd })
+		.then((newUser) => {
+			console.log("New User: ", newUser)
+			res.redirect("/")
+		})}
+	
+
+	});
 });
 
 router.get('/login', (req, res, next) => {
